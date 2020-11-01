ea02f0e7499641f3e3807859e32adf1a0349c9bc
Fernanda Degolin <57374044+fernandadegolin@users.noreply.github.com>
2020-11-01 19:46:56 -0300

    Add files via upload

---
+++ Huia/src/scripts/script.js
@@ -0,0 +1,45 @@
+/* 
+    1)
+    CRM - obrigatorio 
+    CRM - só números
+
+    2)
+    Alerta de campos obrigatorios
+
+    3)
+    Validação de todos os campos 
+        > clicar em continue 
+            > redirecionar para Huia
+
+*/
+
+
+/* 
+
+const botao = document.getElementById('btn');
+    botao.addEventListener('click', clicar);
+    
+
+    function clicar (){
+
+
+        console.log('ta clicando')
+    }
+ */
+
+
+let a = 2;
+
+function sim() {
+    if {
+        a === 8;
+
+    console.log('sim')
+    } else {    
+    console.log('falso')
+}
+
+
+let a = 2
+
+let validar = a === 8? 'true' : 'false';
